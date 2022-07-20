974cad989cc7363ddb59efd20d95afd621a86a6a
injoon149 <dlswns8679@naver.com>
2022-07-20 17:03:38 +0900

    초기 커밋.

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main()
+{
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	vector<pair<int, int>> pv;
+	int a = 0;
+	cin >> a;
+	int b, c;
+	for (int i = 0; i < a; i++)
+	{
+		cin >> b >> c;
+		pv.push_back(make_pair(b, c));
+		sort(pv.begin(), pv.end());
+	}
+	for (auto iter = pv.begin(); iter != pv.end(); iter++)
+	{
+		cout << iter->first << " " << iter->second <<'\n';
+	}
+
+	return 0;
+}
